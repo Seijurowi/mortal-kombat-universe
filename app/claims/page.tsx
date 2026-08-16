bf229638ec8f1031d303b134421fb677cc307d43
Edo <Edgars.Kalva@gmail.com>
2026-08-16 11:20:29 +0300

    Enable static claim query hydration

--- app/claims/page.tsx
+++ app/claims/page.tsx
@@ -3,8 +3,6 @@ import { Suspense } from "react"
 import { ClaimHistoryExplorer } from "@/components/claim-history-explorer"
 import { loadUniverseData } from "@/lib/load-data"
 
-export const dynamic = "force-static"
-
 export default async function ClaimsPage() {
   const data = await loadUniverseData()
 
